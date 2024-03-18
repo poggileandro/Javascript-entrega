397b31a717327b352e65fd40bccc5c16be5ed3d4
LeandroPoggi <poggileandro@gmail.com>
2024-03-18 16:44:40 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,78 +1,157 @@
-const cortesDeCarne = {
-    ASADO: { nombre: "ASADO", precio: 1600 },
-    VACIO: { nombre: "VACIO", precio: 1800 },
-    BONDIOLA: { nombre: "BONDIOLA", precio: 1400 },
-    MATAMBRE: { nombre: "MATAMBRE", precio: 1700 },
-    LOMO: { nombre: "LOMO", precio: 3000 }
-  };
-  
-  let opcion;
-  let cantidad;
-  let gastado;
-  let gastototal = 0;
-  
-  const calcular_gastado = (cant, precio) => {
-      let canttotal = cant * precio;
-      if (cant >= 4) {
-          canttotal -= canttotal * 0.10;
-      }
-      return canttotal;
-  }
-  
-  const cartel = (total, carne) => {
-      alert("Gastaste: $" + total + " en " + carne);
-  }
-  
-  const cartel_final = (gastototal) => {
-      if (gastototal > 0) {
-          alert("Gracias por su Visita. Su Gasto TOTAL es de: $" + gastototal);
-      } else {
-          alert("Gracias por su Visita");
-      }
-  }
-  
-  const pedido_carne = (carne) => {
-      do {
-          cantidad = prompt("CANTIDAD DE " + carne.nombre + " EN KG:\n PRECIO POR KG:$ " + carne.precio + "  \n Si compra 4kg o más se hace un descuento del 10%");
-      } while (cantidad !== null && (isNaN(cantidad) || cantidad <= 0));
-      return cantidad;
-  }
-  
-  const pedido_general = (carne) => {
-      cantidad = pedido_carne(carne);
-      gastado = calcular_gastado(cantidad, carne.precio);
-      cartel(gastado, carne.nombre);
-      gastototal += gastado;
-  }
-  
-  do {
-      do {
-          opcion = prompt("ELIJA EL CORTE DE CARNE A COMPRAR: \n 1 " + cortesDeCarne.ASADO.nombre + "\n 2 " + cortesDeCarne.VACIO.nombre + "\n 3 " + cortesDeCarne.BONDIOLA.nombre + "\n 4 " + cortesDeCarne.MATAMBRE.nombre + "\n 5 " + cortesDeCarne.LOMO.nombre + "\n 0 SALIR");
-      } while (opcion !== null && (isNaN(opcion) || opcion < 0 ));
-  
-      switch (opcion) {
-          case "1":
-              pedido_general(cortesDeCarne.ASADO);
-              break;
-          case "2":
-              pedido_general(cortesDeCarne.VACIO);
-              break;
-          case "3":
-              pedido_general(cortesDeCarne.BONDIOLA);
-              break;
-          case "4":
-              pedido_general(cortesDeCarne.MATAMBRE);
-              break;
-          case "5":
-              pedido_general(cortesDeCarne.LOMO);
-              break;
-          case "0":
-              cartel_final(gastototal);
-              break;
-          default:
-              alert("Número incorrecto");
-              break;
-      }
-  
-  } while (opcion !== "0");
-  
+const nombreYPrecio = [
+    { Nombre: "ASADO", PRECIO: 2000 },
+    { Nombre: "VACIO", PRECIO: 2500 },
+    { Nombre: "BONDIOLA", PRECIO: 3000 },
+    { Nombre: "LOMO", PRECIO: 3500 }
+];
+
+let opcionesMensaje = "";
+let contador = 0;
+nombreYPrecio.forEach(corte => {
+    contador++;
+    opcionesMensaje += contador + " - " + corte.Nombre + " - Precio: $" + corte.PRECIO + "\n";
+});
+
+const cantidadEnKgxCorte = {};
+const precioTotalEnCorte = {};
+
+let opcion;
+let cantidadKG;
+let maxKgVendidos = 0;
+let corteMasVendido = "";
+let cantidadMaxVendida = 0;
+let maxPrecioTotal = 0;
+let corteMasRecaudo = "";
+
+function buscarMaximoVendido() {
+    maxKgVendidos = 0;
+    corteMasVendido = "";
+    cantidadMaxVendida = 0;
+
+    for (let corte in cantidadEnKgxCorte) {
+        let precioTotal = Object.entries(cantidadEnKgxCorte).reduce((total, [nombre, cantidad]) => {
+            const cortePrecio = nombreYPrecio.find(item => item.Nombre === nombre);
+            return total + (cortePrecio.PRECIO * cantidad);
+        }, 0);
+        precioTotalEnCorte[corte] = precioTotal;
+
+        if (cantidadEnKgxCorte[corte] > maxKgVendidos) {
+            maxKgVendidos = cantidadEnKgxCorte[corte];
+            corteMasVendido = corte;
+            cantidadMaxVendida = cantidadEnKgxCorte[corte];
+        }
+    }
+}
+
+function buscarCorteMasRecaudo() {
+    maxPrecioTotal = 0;
+    corteMasRecaudo = "";
+
+    Object.entries(cantidadEnKgxCorte).forEach(([corte, cantidad]) => {
+        let precioTotal = cantidad * nombreYPrecio.find(item => item.Nombre === corte).PRECIO;
+        precioTotalEnCorte[corte] = precioTotal;
+
+        if (precioTotal > maxPrecioTotal) {
+            maxPrecioTotal = precioTotal;
+            corteMasRecaudo = corte;
+        }
+    });
+}
+
+function mensajeMuestraCompleta() {
+        let mensajeCantidad = "Cantidad vendida por corte:\n";
+        let mensajePrecio = "Precio total vendido por corte:\n";
+
+    if (Object.keys(cantidadEnKgxCorte).length === 0) {
+        mensajeCantidad="";
+        mensajePrecio="";
+        mensajeCantidad += "No hay información disponible.\n";
+    } else {
+    
+        for (let corte in cantidadEnKgxCorte) {
+            mensajeCantidad += `${corte}: ${cantidadEnKgxCorte[corte]} kg\n`;
+            mensajePrecio += `${corte}: $${precioTotalEnCorte[corte]}\n`;
+        }
+    }
+    if (mensajeCantidad.trim().length === 0 || mensajePrecio.trim().length === 0) {
+        alert("No hay información disponible para mostrar.");
+    } else {
+        alert(mensajeCantidad + "\n" + mensajePrecio);
+    }
+}
+
+do {
+    opcion = prompt("Ingrese el corte de carne a comprar \n" + opcionesMensaje + "\n---------------- \n SALIR");
+
+    if (/^[a-zA-Z]+$/.test(opcion)) {
+        switch (opcion.toUpperCase()) {
+            case nombreYPrecio[0].Nombre.toUpperCase():
+                cantidadKG = parseFloat(prompt("Has seleccionado " + nombreYPrecio[0].Nombre + "\nEscriba la cantidad a comprar en kg:"));
+                if (!isNaN(cantidadKG)) {
+                    if (nombreYPrecio[0].Nombre in cantidadEnKgxCorte) {
+                        cantidadEnKgxCorte[nombreYPrecio[0].Nombre] += cantidadKG;
+                    } else {
+                        cantidadEnKgxCorte[nombreYPrecio[0].Nombre] = cantidadKG;
+                    }
+                }
+                break;
+            case nombreYPrecio[1].Nombre.toUpperCase():
+                cantidadKG = parseFloat(prompt("Has seleccionado " + nombreYPrecio[1].Nombre + "\nEscriba la cantidad a comprar en kg:"));
+                if (!isNaN(cantidadKG)) {
+                    if (nombreYPrecio[1].Nombre in cantidadEnKgxCorte) {
+                        cantidadEnKgxCorte[nombreYPrecio[1].Nombre] += cantidadKG;
+                    } else {
+                        cantidadEnKgxCorte[nombreYPrecio[1].Nombre] = cantidadKG;
+                    }
+                }
+                break;
+            case nombreYPrecio[2].Nombre.toUpperCase():
+                cantidadKG = parseFloat(prompt("Has seleccionado " + nombreYPrecio[2].Nombre + "\nEscriba la cantidad a comprar en kg:"));
+                if (!isNaN(cantidadKG)) {
+                    if (nombreYPrecio[2].Nombre in cantidadEnKgxCorte) {
+                        cantidadEnKgxCorte[nombreYPrecio[2].Nombre] += cantidadKG;
+                    } else {
+                        cantidadEnKgxCorte[nombreYPrecio[2].Nombre] = cantidadKG;
+                    }
+                }
+                break;
+            case nombreYPrecio[3].Nombre.toUpperCase():
+                cantidadKG = parseFloat(prompt("Has seleccionado " + nombreYPrecio[3].Nombre + "\nEscriba la cantidad a comprar en kg:"));
+                if (!isNaN(cantidadKG)) {
+                    if (nombreYPrecio[3].Nombre in cantidadEnKgxCorte) {
+                        cantidadEnKgxCorte[nombreYPrecio[3].Nombre] += cantidadKG;
+                    } else {
+                        cantidadEnKgxCorte[nombreYPrecio[3].Nombre] = cantidadKG;
+                    }
+                }
+                break;
+            case "SALIR":
+                buscarMaximoVendido();
+                
+                
+                if (corteMasVendido !== "") {
+                    alert("El gasto total de la venta es:" + precioTotalEnCorte[corteMasVendido]);
+                    alert("El corte que más kg vendió es: " + corteMasVendido + " con " + cantidadMaxVendida + " kg vendidos.");
+                } else {
+                    alert("No se ha vendido nada de ningún corte.");
+                }
+                buscarCorteMasRecaudo();
+                if (corteMasRecaudo !== "") {
+                    alert("El corte que más recaudo es: " + corteMasRecaudo + " con una recaudacion total de $" + precioTotalEnCorte[corteMasRecaudo] + ".");
+                } else {
+                    alert("No hay información sobre el corte que mas recaudo.");
+                }
+
+                mensajeMuestraCompleta();
+               
+                alert("Saliendo del programa...");
+                break;
+            default:
+                alert("Por favor, ingrese una opción válida.");
+                break;
+        }
+    } else {
+        alert("Por favor, ingrese solo palabras.");
+    }
+
+} while (opcion.toUpperCase() !== "SALIR");
